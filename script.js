0f9a123e352d0d38f2fbb1851f2422490ad20eb8
Nícolas Marcelo Lima Eloy <134300551+nicolasmleloy@users.noreply.github.com>
2024-12-05 17:14:54 -0400

    Segundo commit

--- script.js
+++ script.js
@@ -13,16 +13,25 @@ async function encontraPersonagem() {
             `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${personagemUser}&ts=1&apikey=e4f36d5e9afff590f757f223a9ce6278&hash=f560c06bdacf8b44a597a7928e159ae5`
         );
         personagem = await personagem.json();
-        var h2 = primeiroPersonagem.childNodes[1];
-        var img = primeiroPersonagem.childNodes[3];
-        var h3 = primeiroPersonagem.childNodes[5];
+        var h2 = primeiroPersonagem.childNodes[1]; //Pega o html do card do primeiro personagem
+        var img = primeiroPersonagem.childNodes[3]; //Pega o html do card do primeiro personagem
+        var h3 = primeiroPersonagem.childNodes[5]; //Pega o html do card do primeiro personagem
 
         let personagemEncontrado = personagem.data.results;
-        h2.innerHTML = personagemEncontrado[0].name;
+        h2.innerHTML = personagemEncontrado[0].name; //Insere o conteudo da API nas tags
         img.src =
             `${personagemEncontrado[0].thumbnail.path}.` +
-            `${personagemEncontrado[0].thumbnail.extension}`;
-        h3.innerHTML = personagemEncontrado[0].description;
+            `${personagemEncontrado[0].thumbnail.extension}`; //Insere o conteudo da API nas tags
+        h3.innerHTML = personagemEncontrado[0].description; //Insere o conteudo da API nas tags
+
+        if (personagem.data.results.length > 1) {
+            var outrosPersonagens = document.createElement("div"); // Tentando criar os outros personagem para inserir os dados
+            outrosPersonagens.innerHTML = `<div id="primeiro-personagem"> 
+            <h2 class="nome-personagem"></h2>
+            <img class="img-personagem" src="" alt="" />
+            <h3 class="descricao-personagem"></h3>
+            </div>`;
+        }
 
         for (let i = 0; i < personagemEncontrado.length; i++) {
             console.log(personagemEncontrado[i]);
